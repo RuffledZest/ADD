4680ed06b9eed8842e3e39457ccd00257d2f9d38
hardikchopra18 <139274403+hardikchopra18@users.noreply.github.com>
2023-10-21 09:34:47 +0530

    completed till main page except for the add skill part

--- Components/Mainpage.js
+++ Components/Mainpage.js
@@ -1,13 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 
 const Mainpage = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Mainpage</Text>
     </View>
   )
 }
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  }
+
+
+});
 
 
 export default Mainpage
--- Components/Register3.js
+++ Components/Register3.js
@@ -7,12 +7,11 @@ const Register3 = ({navigation}) => {
     const register2handler=()=>{
         navigation.navigate("register2");
     }
+    const mainpagehandler=()=>{
+        navigation.navigate("mainpage");
+    }
   return (
     <View style={styles.background} >
-        {/* bg Img */}
-        {/* <View style={styles.bgImg}>
-          <Image  source={require("../assets/dleaf.png")}/>
-        </View> */}
 
       {/* title logo */}
       <View style={styles.mainLogocontainer}>
@@ -21,48 +20,90 @@ const Register3 = ({navigation}) => {
         </TouchableOpacity>
         <Image style={styles.mainlogo} source={require("../assets/mainlogo.png")}/>
       </View>
+
         {/* text */}
         <View>
             <Text  style={styles.setprofileText}>Add Skills</Text>
-         </View>
-
+        </View>
+           
       {/* input fields */}
-      <View style={styles.inputViewparent}>
-        <Text style={styles.labelText}>Username</Text>
-        <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Enter your Email/ Phone Number"
-          placeholderTextColor="#003f5c"
-        />
+      
+      {/* linkdein */}
+      <View style={styles.field}>
+        {/* image and text */}
+        <View style={styles.imagetxt}>
+        {/* Image */}
+         <View style={styles.img}>
+          <Image style={styles.linkdein} source={require("../assets/lindkein.png")}/>
+         </View>
+          {/* text */}
+         <View style={styles.txts}>
+          <Text style={styles.labelText}>Linkdein</Text>
+         </View>
         </View>
-      </View> 
-      <View style={styles.inputViewparent}>
-        <Text style={styles.labelText}>Password</Text>
+
         <View style={styles.inputView}>
         <TextInput
           style={styles.inputText}
-          placeholder="Characters must be between 8-16"
-          placeholderTextColor="#003f5c"
-        />
-        </View>
-      </View> 
-      <View style={styles.inputViewparent}>
-        <Text style={styles.labelText}>Confirm Password</Text>
-        <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Re-enter your Password"
+          placeholder="Enter Your Linkedln Profile ID"
           placeholderTextColor="#003f5c"
         />
         </View>
       </View> 
       
-      {/* next button */}
-      <TouchableOpacity style={styles.registerBtn}  >
-        <Text style={styles.registerText} >NEXT</Text>
-      </TouchableOpacity>
+      {/* Github id */}
+      <View style={styles.field}>
+        {/* image and text */}
+        <View style={styles.imagetxt}>
+        {/* Image */}
+         <View style={styles.img}>
+          <Image style={styles.linkdein} source={require("../assets/github.png")}/>
+         </View>
+          {/* text */}
+         <View style={styles.txts}>
+          <Text style={styles.labelText}>Github</Text>
+         </View>
+        </View>
 
+        <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter Your Github Profile ID"
+          placeholderTextColor="#003f5c"
+        />
+        </View>
+      </View> 
+      
+      {/* Discord id */}
+      <View style={styles.field}>
+        {/* image and text */}
+        <View style={styles.imagetxt}>
+        {/* Image */}
+         <View style={styles.img}>
+          <Image style={styles.linkdein} source={require("../assets/discord.png")}/>
+         </View>
+          {/* text */}
+         <View style={styles.txts}>
+          <Text style={styles.labelText}>Discord Id</Text>
+         </View>
+        </View>
+
+        <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter Your Discord Profile ID"
+          placeholderTextColor="#003f5c"
+        />
+        </View>
+      </View> 
+      
+      
+      {/* next button */}
+      <TouchableOpacity onPress={mainpagehandler} style={styles.registerBtn}  >
+        <Text style={styles.registerText}  >NEXT</Text>
+      </TouchableOpacity>
+        
+        {/* Description */}
     <View style={styles.bottomtextContainer}>
         <Text style={styles.bottomtext}>By signing up,you agree to use this app's Terms of Services and Privacy Policy</Text>
     </View>
@@ -73,7 +114,34 @@ const Register3 = ({navigation}) => {
   )
 }
 const styles = StyleSheet.create({
-    background:{
+  field:{
+       marginBottom:10
+  } , 
+  imagetxt:{
+     display: "flex",
+    //  borderWidth:2,
+    // borderColor:"#FF0000",
+    //  gap:10,
+     padding:5,
+     alignItems:"center",
+     flexDirection:"row"
+    },
+    img:{
+      marginLeft:20,
+      width:35,
+      height:35
+    },
+    linkdein:{
+      width:"100%",
+      height:"100%"
+      
+    },
+
+   
+    txts:{
+       marginLeft:-5
+    },
+  background:{
         backgroundColor:"#FFFFFF",
         height:"100%"
     },
@@ -89,10 +157,14 @@ const styles = StyleSheet.create({
         marginLeft:15,
         height:25
       },
-    setprofileText:{
+      setprofileText:{
        fontSize:40,
        marginLeft:8,
        marginTop:10,
+      //  borderColor:"#000000",
+      //  borderWidth:2,
+      //  fontFamily:"Arial, sans-serif",
+       marginBottom:20,
     //    borderColor:"#000000",
     //    borderWidth:2,
        
@@ -101,6 +173,7 @@ const styles = StyleSheet.create({
         display:"flex",
         flexDirection:"row",
         alignItems:"center",
+        // marginBottom:5,
         // justifyContent:"space-between",
         marginTop:55,
         width:"100%",
@@ -122,8 +195,6 @@ const styles = StyleSheet.create({
         top:300,
         left:30,
         // transform: [{rotate: '-18deg'}],
-        // borderColor:"#000000",
-        // borderWidth:2,
         display:"flex",
         alignItems:"center",
         justifyContent:"center",
@@ -150,13 +221,13 @@ const styles = StyleSheet.create({
        display:"flex",
        width: '90%',
        marginLeft:15
-     },
-     inputText: {
-       borderRadius:25,
-       borderWidth:2,
-       padding:15,
-       height: 50,
-       width:"100%",
+      },
+      inputText: {
+        borderRadius:25,
+        borderWidth:2,
+        padding:15,
+        height: 50,
+        width:"100%",
        
      },
      registerBtn: {
